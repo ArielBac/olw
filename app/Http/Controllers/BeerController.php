bd6440989aea38ac9d082b5a7d48964a9c95d8aa
ArielBac <arielvieira65@gmail.com>
2022-10-19 23:33:04 -0300

    Add screens vue, update controllers and web.php, add some required packages

--- app/Http/Controllers/BeerController.php
+++ app/Http/Controllers/BeerController.php
@@ -6,14 +6,24 @@ use App\Http\Requests\BeerRequest;
 use App\Jobs\ExportJob;
 use App\Jobs\SendExportEmailJob;
 use App\Jobs\StoreExportDataJob;
+use App\Models\Meal;
 use App\Services\PunkapiService;
+use Inertia\Inertia;
 
 class BeerController extends Controller
 {
     public function index(BeerRequest $request, PunkapiService $service)
     {
         // Os ... (spread operator) é usado para passar os parâmetros do request para o método, ele associa o nome do parâmetro com o valor
-        return $service->getBeers(...$request->validated()); // validated() passa apenas os parâmetros validados pelo FormRequest
+        $beers = $service->getBeers(...$request->validated()); // validated() passa apenas os parâmetros validados pelo FormRequest
+        $meals = Meal::all();
+        $filters = $request->validated();
+
+        return Inertia::render('Beers', [
+            'beers' => $beers,
+            'meals' => $meals,
+            'filters' => $filters,
+        ]);
     }
 
     public function export(BeerRequest $request, PunkapiService $service)
@@ -26,6 +36,7 @@ class BeerController extends Controller
             new StoreExportDataJob(auth()->user(), $filename)
         ])->dispatch($request->validated(), $filename);
 
-        return 'relatorio criado';
+        return redirect()->back()
+            ->with('success', 'Exportação iniciada, você receberá um e-mail quando ela estiver pronta.');
     }
 }
